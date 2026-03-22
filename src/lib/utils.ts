328a52baf2e6dd162e085e437452d97c4d21d934
Foster <fosterjameskiatarae@gmail.com>
2026-03-22 21:35:23 +1300

    feat: add comment to clarify cn function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// Function to allow using lists for classNames
 export const cn = (...inputs: any[]) => {
   return twMerge(clsx(inputs));
 };
